b61550ad2300980abe6bb6be309f1a8af7ce85f8
Fernando Mendoza <fernandoza@gmail.com>
2019-08-22 14:24:37 -0500

    feat: add enough code to see the test fail

---
+++ src/main/java/CodeupCrypt.java
@@ -0,0 +1,16 @@
+public class CodeupCrypt {
+    private String password;
+    private String hash;
+
+    public CodeupCrypt(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+}
--- src/test/java/CryptTest.java
+++ src/test/java/CryptTest.java
@@ -2,5 +2,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CryptTest {
-
+    @Test
+    public void testHasPasswordAndHash(){
+        CodeupCrypt crypt = new CodeupCrypt("strongPass");
+        assertEquals(crypt.getPassword(), "strongPass");
+        assertEquals(crypt.getHash(), "str0ngP4ss");
+    }
 }
